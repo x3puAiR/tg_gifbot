b3250fef2b2da003b783dd13fe9580a4e26bbff0
x3puAiR <42696743+x3puAiR@users.noreply.github.com>
2025-10-24 15:16:18 +0800

    Add webm sticker support (#1)
    * fix webm sticker download err
    
    * update docker-compose for build only instead of pull from GA
    
    * add logs
    
    * remove stupid emojis from AI

--- main_run_bot.py
+++ main_run_bot.py
@@ -7,10 +7,14 @@ from telegram_bot.bot_executor import BotExecutor
 
 
 if __name__ == '__main__':
+    print('Initializing Telegram GIF Bot...')
+    print('Loading configuration and dependencies...')
+    
     # cert_path = os.path.join(_base_dir, '.openssl/cert.pem')
     # key_path = os.path.join(_base_dir, '.openssl/private.key')
     # webhook_url = 'https://YOUR_DOMAIN:8443/gifbot'
     # BotExecutor(cert_path=cert_path,
     #             key_path=key_path,
     #             webhook_url=webhook_url).execute()
+    
     BotExecutor().execute()
